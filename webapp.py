5cce12706903aded7b80dbc85d89d16bd0f4b813
P3 <angel9@gmail.com>
2019-05-16 11:07:52 -0700

    cleaned code

--- webapp.py
+++ webapp.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, redirect, url_for, session, request, jsonify, Markup
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup, render_template
 from flask_oauthlib.client import OAuth
-from flask import render_template
 from time import localtime, strftime
 from bson.objectid import ObjectId
 from threading import Lock
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
+
+#===============================================================================
 
 import pprint
 import os
@@ -14,6 +14,8 @@ import json
 import pymongo
 import dns
 import sys
+
+#===============================================================================
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=None)
@@ -24,12 +26,15 @@ thread1_lock = Lock()
 thread2 = None
 thread2_lock = Lock()
 
+#===============================================================================
+
 app.debug = True #Change this to False for production
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
 
+#===============================================================================
 
-    #Set up GitHub as OAuth provider
+#Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
     consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
@@ -42,17 +47,16 @@ github = oauth.remote_app(
     authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
-#TODO: globalVar=postData Create and set a global variable for the name of you JSON file here.  The file will be storedd on Heroku, so you don't need to make it in GitHub
-#TODO: Create the file on Heroku using os.system.  Ex) os.system("echo '[]'>"+myFile) puts '[]' into your file
-#os.system("echo '[]'>"+pdata)
+#===============================================================================
 
 def background_thread1():
     count=0
     while True:
         socketio.sleep(5) #wait 5 seconds
         count=count+1
-<<<<<<< HEAD
         socketio.emit('count_event1', count) #sends out the varible count to all of the cleints
+
+#===============================================================================
 
 def background_thread2():
     count=0
@@ -60,31 +64,28 @@ def background_thread2():
         socketio.sleep(5) #wait 5 seconds
         count=count+1
         socketio.emit('count_event2', count) #sends out the varible count to all of the cleints
-
-=======
         socketio.emit('count_event', count) #sends out the varible count to all of the cleints
-        
-        
-        
-        # if client 1 = 'Rock' and client 2 = 'Paper': print client 2 won 
-        # if client 1 = 'Paper' and client 2 = 'Rock': print client 1 won 
-        # if client 1 = 'Scissors' and client 2 = 'Paper': print client 1 won 
-        # if client 1 = 'Paper' and client 2 = 'Scissors': print client 2 won 
-        # if client 1 = 'Rock' and client 2 = 'Scissors': print client 1 won 
-        # if client 1 = 'Scissors' and client 2 = 'Rock': print client 2 won 
-        
-        
-        
-        
+
+
+
+        # if client 1 = 'Rock' and client 2 = 'Paper': print client 2 won
+        # if client 1 = 'Paper' and client 2 = 'Rock': print client 1 won
+        # if client 1 = 'Scissors' and client 2 = 'Paper': print client 1 won
+        # if client 1 = 'Paper' and client 2 = 'Scissors': print client 2 won
+        # if client 1 = 'Rock' and client 2 = 'Scissors': print client 1 won
+        # if client 1 = 'Scissors' and client 2 = 'Rock': print client 2 won
+
+
+
+
         #win=request.form["Rock"] win=request.form["Paper"] win=request.form["Scissors"]
-        
-       
->>>>>>> e9f86b0174739dfa101202db835820e1f050a0fa
+
+#===============================================================================
+
 @socketio.on('connect')
 def test_connect():
     global usernum
     with usernum_lock:
-<<<<<<< HEAD
         print(usernum)
         if usernum >= 2:
             if session['user_data']['login'] == '':
@@ -95,28 +96,28 @@ def test_connect():
                     if thread2 is None:
                         thread2=socketio.start_background_task(target=background_thread2)
                         socketio.to('room2').emit('connection2', 'connected')# this is the message that goes along with start in the JQuery code
-                        usernum=1
                     usernum=usernum+1
-                    print('here2')
-=======
-        if usernum == 2:
-            return redirect(url_for(".home"))
->>>>>>> e9f86b0174739dfa101202db835820e1f050a0fa
+                    print('function 2')
         else:
             if session['user_data']['login'] == '':
                 yeet='yeet'
             else:
-                global thread1 #this is a global varible which is the same across all cleints
-                with thread1_lock: #locks the global varible so only one client can use it at a time
+                global thread1
+                with thread1_lock:
                     if thread1 is None:
                         thread1=socketio.start_background_task(target=background_thread1)
                         socketio.to('room1').emit('connection1', 'connected')# this is the message that goes along with start in the JQuery code
                         usernum=1
                     usernum=usernum+1
-                    print('here1')
+                    print('function 1')
+
+#===============================================================================
+
 @app.context_processor
 def inject_logged_in():
     return {"logged_in":('github_token' in session)}
+
+#===============================================================================
 
 @app.route('/')
 def home():
@@ -126,48 +127,47 @@ def home():
     except:
         return render_template('Home.html')
 
+#===============================================================================
+
 @app.route('/p3')
 def StartGame():
     return render_template('StartGame.html')
-    
-@app.route('/button', methods=['POST'])
-def Button():   
-    
-    if 'Rock' in request.form: 
-        print("rock")
-   
-    if 'Paper'in request.form: 
-        print("paper")
-   
-    if 'Scissors' in request.form: 
-        print("scissors")
-    return redirect(url_for("StartGame"))
+
+#===============================================================================
 
 @app.route('/button', methods=['POST'])
 def Button():
-    print(request.form)
+
     if 'Rock' in request.form:
         print("rock")
-    print(request.form)
+
     if 'Paper'in request.form:
         print("paper")
-    print(request.form)
-    if 'Scissors' in request.form: 
+
+    if 'Scissors' in request.form:
         print("scissors")
     return redirect(url_for("StartGame"))
+
+#===============================================================================
 
 @app.route('/p2')
 def Info():
     return render_template('Info.html')
 
+#===============================================================================
+
 @app.route('/login')
 def login():
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+
+#===============================================================================
 
 @app.route('/logout')
 def logout():
     session.clear()
     return render_template('Home.html', message='You were logged out')
+
+#===============================================================================
 
 @app.route('/login/authorized')
 def authorized():
@@ -186,10 +186,14 @@ def authorized():
             message='Unable to login, please try again.  '
     return render_template('Home.html', message=message)
 
+#===============================================================================
+
 #the tokengetter is automatically called to check who is logged in.
 @github.tokengetter
 def get_github_oauth_token():
     return session.get('github_token')
+
+#===============================================================================
 
 if __name__ == '__main__':
     os.system("echo json(array) > file")
